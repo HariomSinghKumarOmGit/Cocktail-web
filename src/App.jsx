62ad3562c79de41b4c29b9fe3b4f840f9f38d675
Hariom Singh <suppraaaa2003@gmail.com>
2026-01-14 21:51:01 +0530

    app setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import gsap from 'gsap'
+import {ScrollTrigger, SplitText} from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
 import React from 'react';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="flex-center h-[100vh]   ">
+      <h1 className="text-3xl text-indigo-300">YooooMeta mera beta</h1>
+    </div>
   );
 };
 
