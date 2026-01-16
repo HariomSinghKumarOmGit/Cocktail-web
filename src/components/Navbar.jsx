e2d4c1ba2849b7983658db0b9e6a973c27d9eb53
Hariom Singh <suppraaaa2003@gmail.com>
2026-01-16 05:54:10 +0530

    web done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
           <div>
               <a href="#home" className = "flex items-center gap=2 "   >
                   <img src="/images/logo.png" alt="logo"/>
-                  <p>Paasiaano Flick</p>
+                  <p>Aiova Flick</p>
               </a>
 
               <ul>
